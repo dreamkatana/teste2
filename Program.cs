6a9407dc7f433f2dbd084d2ffde093e7ff96b764
Santos <joaoedu@gmail.com>
2022-09-07 14:42:53 -0300

    first commit

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+//string meustring = Console.ReadLine();
+//int n = int.Parse(meustring);
+//Console.WriteLine(n);
+
+int i = 0;
+while(i < 3)
+{
+Console.WriteLine(i);
+i++;
+}
+
+foreach(char c in "beer") // c é a variável de iteração
+    Console.WriteLine(c);
+
+
+Console.WriteLine("C1");
