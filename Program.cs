3d88af5004ea4dc275dc6048bc16a3ae7575b245
Santos <joaoedu@gmail.com>
2022-09-08 15:33:13 -0300

    COMENTARIO C6

--- Program.cs
+++ Program.cs
@@ -19,9 +19,16 @@ foreach(char c in "beer") // c é a variável de iteração
 Console.WriteLine("C1");
 //COMENTARIO C2
 Console.WriteLine("C2");
+<<<<<<< HEAD
 //COMENTARIO C3
 Console.WriteLine("C3");
 //COMENTARIO C8
 Console.WriteLine("C8");
 //COMENTARIO C9
 Console.WriteLine("C9");
+=======
+//COMENTARIO C5
+Console.WriteLine("C5");
+//COMENTARIO C5
+Console.WriteLine("C6");
+>>>>>>> b28848a (COMENTARIO C6)
